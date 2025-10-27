1187cff8aa67ba84eb78567bf11acfb4e7f549d6
VarunMudaliar2607 <varunsaravananmudaliarbca24@rvu.edu.in>
2025-10-27 10:32:10 +0530

    Update lab5.sql

--- lab5.sql
+++ lab5.sql
@@ -1,8 +1,8 @@
 -- Lab Experiment 04: Implement arithmetic, logical, comparison, special, and set operators in SQL using the Employees and Departments tables.
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------
--- STUDENT NAME: GAURAV P RAO
--- USN: 1RUA24BCA0027
+-- STUDENT NAME: varun
+-- USN: 1RUA24BCA0096
 -- SECTION: A
 -- -----------------------------------------------------------------------------------------------------------------------------------------
 SELECT USER(), 
